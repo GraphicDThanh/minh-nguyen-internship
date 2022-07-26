164bc592a1055ca8b70f9d21401efaae8ac994e5
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-07-26 17:52:47 +0700

    feat: example types

---
+++ javascript/examples/types.js
@@ -0,0 +1,18 @@
+//string
+const name = 'ram';
+const name1 = "hari";
+const result = `The names are ${name} and ${name1}`;
+document.write(result);
+
+//number
+const number1 = 3;
+const number2 = 3.433;
+const number3 = 3e5 // 3 * 10^5
+document.writeln(number1 + number2 + number3);
+
+const number4 = 3/0;
+document.writeln(number4); // Infinity
+
+const number5 = -3/0;
+document.writeln(number5); // -Infinity
+
