3207938c1c085bfded86cf6c6feb01c2b3c93186
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-07-26 18:15:24 +0700

    feat: example expressions

---
+++ javascript/examples/expressions.js
@@ -0,0 +1,14 @@
+// Arithmetic expressions
+const a = 5;
+const b = 3;
+document.writeln(a | b);
+
+// String expressions
+let A = 'abc';
+document.writeln(A+"d");
+
+// Logical expressions
+const c = 5, d = 2, e = 'hello';
+document.writeln(a == 5);
+document.writeln(b == '2');
+document.writeln(c == 'Hello');
