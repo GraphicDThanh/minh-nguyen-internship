72c9630524f572c3020e3f6623e08e3806be69fa
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-07-27 14:26:28 +0700

    feat: #38 example comparisons

---
+++ javascript/examples/comparisons.js
@@ -0,0 +1,10 @@
+// Equal to
+const a1 = 5;
+document.writeln(a1 == 5);
+document.writeln("<br/>");
+// Not Equal
+document.writeln(a1 != 5);
+document.writeln("<br/>");
+// Strict Equal
+document.writeln(a1 === 2);
+document.writeln("<br/>");
