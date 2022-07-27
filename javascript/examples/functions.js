b1940aabab951104c87e6ab625164e62b032b079
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-07-27 15:37:08 +0700

    feat: #38 example functions

---
+++ javascript/examples/functions.js
@@ -0,0 +1,8 @@
+function getData(color, age) {
+  document.writeln("color: " + color);
+  document.writeln("<br>");
+  document.writeln("age: " + age);
+  document.writeln("<br>");
+}
+getData("green", 24);
+getData("black");
--- javascript/examples/loops.js
+++ javascript/examples/loops.js
@@ -1,7 +1,25 @@
 const list = ["a", "b", "c"];
 let i = 0;
+
+//while
+document.writeln("Use while loop: ");
 while (i < list.length) {
   document.writeln(list[i]); //value
   document.writeln(i); //index
   i = i + 1;
+}
+document.writeln("<br>");
+
+//for
+document.writeln("Use for: ");
+for (let i = 0; i < list.length; i++) {
+  document.writeln(list[i]); //value
+  document.writeln(i); //index
+}
+document.writeln("<br>");
+
+//for...of
+document.writeln("Use for..of: ");
+for (const value of list) {
+  document.writeln(value); //value
 }
