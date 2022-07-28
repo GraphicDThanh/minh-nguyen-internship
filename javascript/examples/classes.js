04648db5af039738680fe3aa54bd97c6d58b9b8e
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-07-28 08:53:41 +0700

    fix: #38 example classes

---
+++ javascript/examples/classes.js
@@ -0,0 +1,21 @@
+//Case 1: no function arguments
+class Person {
+  hello() {
+    return "Hello, I am Flavio";
+  }
+}
+const flavio = new Person();
+document.writeln("Case 1: " + flavio.hello());
+document.writeln("<br>");
+
+//Case 2: have a function argument
+class Person1 {
+  constructor(name) {
+    this.name = name;
+  }
+  hello() {
+    return "Hello, I am " + this.name + ".";
+  }
+}
+const flavio1 = new Person1('flavio')
+document.writeln("Case 2: " + flavio.hello());
