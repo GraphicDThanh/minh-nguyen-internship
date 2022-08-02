c711ccc6f66007a4d000eb7fa3488d5f51d79659
minhng271 <minhng271@gmail.com>
2022-08-02 15:04:29 +0700

    fix: #40 add comment

--- javascript/examples/functions.js
+++ javascript/examples/functions.js
@@ -5,5 +5,8 @@ function getData(color, age) {
   document.writeln('<br>');
 }
 
+// Passing 2 arguments and print 'green' and '24'
 getData('green', 24);
+
+// Missing param fos the age then will print 'underfined'
 getData('black');
