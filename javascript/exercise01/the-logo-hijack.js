e386e8ab56e31b7d42d949491d5c6c90c9af0a40
minhng271 <minhng271@gmail.com>
2022-08-02 17:22:58 +0700

    fix: #42 add comment and rename id

--- javascript/exercise01/the-logo-hijack.js
+++ javascript/exercise01/the-logo-hijack.js
@@ -1,5 +1,9 @@
-var img = document.getElementById("hplogo");
+var img = document.getElementById("logo");
 img.width = 500;
+
+// Find the Google logo and store it in a variable.
 img.src = "http://www.logostage.com/logos/yahoo.GIF";
-var button = document.getElementById("gbqfba");
+var button = document.getElementById("print");  
+
+// Modify the text of the button so that it says "Yahooo!" instead.
 button.innerHTML = "Yahoooo!";
