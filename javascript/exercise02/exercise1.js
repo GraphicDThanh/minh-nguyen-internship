dea2e3379ca962df49a34c3826f33673a76c90b6
minhng271 <minhng271@gmail.com>
2022-08-03 17:07:30 +0700

    feat: #46 exercise 1

---
+++ javascript/exercise02/exercise1.js
@@ -0,0 +1,8 @@
+// font styles added by JS:
+function setStyle() {
+  const text = document.getElementById('text-example');
+
+  text.style.fontSize = '14px';
+  text.style.fontFamily = 'Comic Sans MS';
+  text.style.color = 'green';
+}
