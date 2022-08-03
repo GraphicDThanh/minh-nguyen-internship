92c5fbe7789ade220e1c9245a4d7d526fd06e7b8
minhng271 <minhng271@gmail.com>
2022-08-03 14:36:15 +0700

    fix: switch to using const

--- javascript/exercise01/the-logo-hijack.js
+++ javascript/exercise01/the-logo-hijack.js
@@ -1,9 +1,9 @@
-var img = document.getElementById("logo");
+const img = document.getElementById('logo');
 img.width = 500;
 
 // Find the Google logo and store it in a variable.
-img.src = "http://www.logostage.com/logos/yahoo.GIF";
-var button = document.getElementById("print");  
+img.src = 'http://www.logostage.com/logos/yahoo.GIF';
+const button = document.getElementById('print');
 
 // Modify the text of the button so that it says "Yahooo!" instead.
-button.innerHTML = "Yahoooo!";
+button.innerHTML = 'Yahoooo!';
