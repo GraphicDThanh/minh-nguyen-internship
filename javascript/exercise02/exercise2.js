84d85f1a51260fc3cb9633065fbfc561c56dd64d
minhng271 <minhng271@gmail.com>
2022-08-03 17:07:47 +0700

    feat: #46 exercise 2

---
+++ javascript/exercise02/exercise2.js
@@ -0,0 +1,9 @@
+function getFormValue() {
+  const x = document.getElementById('form1');
+
+  for (let i = 0; i < x.length; i += 1) {
+    if (x.elements[i].value !== 'Submit') {
+      document(x.elements[i].value);
+    }
+  }
+}
