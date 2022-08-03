5d8262bcfca242bad6755ebd2b5e4f649e7f33ad
minhng271 <minhng271@gmail.com>
2022-08-03 14:41:38 +0700

    fix(about-me): #42 switch to using const

--- javascript/exercise01/about-me.js
+++ javascript/exercise01/about-me.js
@@ -1,19 +1,20 @@
 // Change the body style so it has a font-family of "Arial, sans-serif".
-document.body.style.fontFamily = "Arial, sans-serif";
+document.body.style.fontFamily = 'Arial, sans-serif';
 
 // Replace each of the spans (nickname, favorites, hometown) with your own information.
-document.getElementById("name").innerHTML = "Minh Nguyen";
-document.getElementById("favorites").innerHTML = "listen to music";
-document.getElementById("hometown").innerHTML = "Da Nang";
+document.getElementById('name').innerHTML = 'Minh Nguyen';
+document.getElementById('favorites').innerHTML = 'listen to music';
+document.getElementById('hometown').innerHTML = 'Da Nang';
 
-// Iterate through each li and change the class to "listitem". Add a style tag that sets a rule for "listitem" to make the color red
-var items = document.getElementsByTagName("li");
-for (var i = 0; i < items.length; i++) {
-  items[i].className = "listitem";
+// Iterate through each li and change the class to "listitem".
+const items = document.getElementsByTagName('li');
+for (let i = 0; i < items.length; i += 1) {
+  items[i].className = 'listitem';
+  // Add a style tag that sets a rule for "listitem" to make the color red.
+  items[i].style.color = 'red';
 }
 
-// Create a new img element and set its src attribute to a picture of you. Append that element to the page.
-var myPic = document.createElement("img");
-myPic.src =
-  "https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo-shadow.png";
+// Create a new img element and set its src attribute to a picture of you.
+const myPic = document.createElement('img');
+myPic.src = 'https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo-shadow.png';
 document.body.appendChild(myPic);
