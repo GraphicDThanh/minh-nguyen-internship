cab358e87abb0fa073983b1899be979a19b3dcc6
minhng271 <minhng271@gmail.com>
2022-08-03 14:40:22 +0700

    feat: #42 create the-book-list

---
+++ javascript/exercise01/the-book-list.js
@@ -0,0 +1,43 @@
+// Create array of books
+const books = [
+  {
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    alreadyRead: false,
+  },
+  {
+    title: 'Conan',
+    author: 'Aoyama',
+    alreadyRead: true,
+  }];
+
+//  Create p element with the book title and author and append it to the page.
+for (let i = 0; i < books.length; i += 1) {
+  const bookP = document.createElement('p');
+  const bookDescription = document.createTextNode(`${books[i].title} by ${books[i].author}`);
+  bookP.appendChild(bookDescription);
+  document.body.appendChild(bookP);
+}
+
+// Bonus: Use a ul and li to display the books.
+const bookList = document.createElement('ul');
+for (let i = 0; i < books.length; i += 1) {
+  const bookItem = document.createElement('li');
+
+  // Bonus: Add an img element for each book on the page.
+  const bookImg = document.createElement('img');
+  bookImg.src = books[i].img;
+  bookItem.appendChild(bookImg);
+
+  // Book title and author and append it to the page.
+  const bookDescription = document.createTextNode(` ${books[i].title} by ${books[i].author}`);
+  bookItem.appendChild(bookDescription);
+
+  // Bonus: Change the style of the book depending on whether you have read it or not.
+  if (books[i].alreadyRead === false) {
+    bookItem.style.color = 'red';
+  }
+  bookList.appendChild(bookItem);
+}
+
+document.body.appendChild(bookList);
