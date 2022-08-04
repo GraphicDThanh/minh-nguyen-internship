d4e6cc995ca76fc3e3c39dcaca32a15d05f6244d
minhng271 <minhng271@gmail.com>
2022-08-04 17:17:46 +0700

    feat: #46 exercise 7

---
+++ javascript/exercise02/exercise7.js
@@ -0,0 +1,12 @@
+function createTable() {
+  const row = document.getElementById('numberRow').value;
+  const column = document.getElementById('numberColumn').value;
+
+  for (let r = 0; r < row; r += 1) {
+    const x = document.getElementById('myTable').insertRow(r);
+    for (let c = 0; c < column; c += 1) {
+      const y = x.insertCell(c);
+      y.innerHTML = `Row-${r} Column-${c}`;
+    }
+  }
+}
