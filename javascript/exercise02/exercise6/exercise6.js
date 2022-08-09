e9090fcf3bbdb5e6225775388210ba13c8431794
minhng271 <minhng271@gmail.com>
2022-08-09 17:14:10 +0700

    fix: create file js

--- javascript/exercise02/exercise6/exercise6.js
+++ javascript/exercise02/exercise6/exercise6.js
@@ -1,6 +1,20 @@
-function changeContent() {
-    console
-  const cell = document.querySelector('.value');
-  console.log(cell);
-  const content = cell.value;
-}
+const form = document.querySelector('.form');
+const items = document.querySelectorAll('.value');
+
+// Get data from selected cell
+items.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    const text = e.target.innerHTML;
+    document.querySelector('#content').value = text;
+    e.target.classList.add('action');
+  });
+});
+
+// Click submit button to push the edited data to the selected cell
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const editedText = document.querySelector('.action');
+  const { value } = document.querySelector('#content');
+  editedText.innerHTML = value;
+  editedText.classList.toggle('action');
+});
