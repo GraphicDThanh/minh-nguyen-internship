d957484275a28de960b443880ce13ad47970b201
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-08-10 17:08:07 +0700

    fix: #46 fix export input data from form

--- javascript/exercise02/exercise10/exercise10.js
+++ javascript/exercise02/exercise10/exercise10.js
@@ -1,14 +1,16 @@
-function volumeSphere() {
-  let volume;
+function volumeSphere()
+ {
+  //get the absolute value of the input
   let radius = document.getElementById('radius').value;
-
   radius = Math.abs(radius);
-  volume = (4 / 3) * Math.PI * radius ** 3;
-  volume = volume.toFixed(4);
-  document.getElementById('volume').value = volume;
-  return false;
-}
 
-document.getElementById('MyForm').addEventListener('submit', () => {
-  document.writeln(volumeSphere().volume);
-});
+  // use the formula to calculate the volume of the sphere
+  let volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  // take 4 decimal places
+  volume = volume.toFixed(4);
+
+  // print the result to the volume cell
+  let result = document.getElementById('volume');
+  result.value = volume;
+ }
+ window.onload = document.getElementById('MyForm').onsubmit = volumeSphere;
