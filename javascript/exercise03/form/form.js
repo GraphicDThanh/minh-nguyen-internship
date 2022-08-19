d00e952e2585600d23cc48ac6c31b82f529bafd6
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-08-19 19:10:59 +0700

    fix: #50 add comment

--- javascript/exercise03/form/form.js
+++ javascript/exercise03/form/form.js
@@ -6,15 +6,20 @@ const rePass = document.getElementById("repass");
 // Validate Email
 function validateEmail(input) {
   const msgEmail = document.getElementById("msg_email");
+
+  // case 1: leave the input field blank
   if (input.value === "") {
     msgEmail.innerText = "Email is empty";
     return false;
   } else {
     var validRegex = /^[a-zA-Z0-9.]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    // case 2: enter the correct email format
     if (input.value.match(validRegex)) {
       msgEmail.innerText = "";
       return true;
-    } else {
+    } 
+    //case 3: enter the wrong email format
+    else { 
       msgEmail.innerText =
         "Email address wrong format. example: username@somewhere.sth";
       return false;
@@ -25,18 +30,27 @@ function validateEmail(input) {
 // Validate Username
 function validateUsername(input) {
   const msgName = document.getElementById("msg_name");
+
+  // case 1: leave the input field blank
   if (input.value === "") {
     msgName.innerText = "Username is empty";
     return false;
-  } else if (input.value.length > 40) {
+  } 
+  
+  // case 2: input content should not exceed 40 characters
+  else if (input.value.length > 40) {
     msgName.innerText = "Username is too long. Maximum is 40 character";
     return false;
   } else {
     const validRegex = /^[a-zA-Z0-9_-]*$/;
+
+    // case 3: names accepted with limited characters accepted
     if (input.value.match(validRegex)) {
       msgName.innerText = "";
       return true;
-    } else {
+    } 
+    // case 4: contain strange symbols or extra spaces
+    else {
       msgName.innerText =
         "Username is invalid. Be sure it does not contain strange symbols or extra spaces anywhere";
       return false;
@@ -47,15 +61,22 @@ function validateUsername(input) {
 // Validate Password
 function validatePassword(input) {
   const msgPass = document.getElementById("msg_pass");
+
+  // case 1: leave the input field blank
   if (input.value == "") {
     msgPass.innerText = "Password is empty";
     return false;
   } else {
     var validRegex = /^[a-zA-Z0-9]*$/;
+
+    // case 2: valid password with allowed characters
     if (input.value.match(validRegex)) {
       msgPass.innerText = "";
       return true;
-    } else {
+    } 
+    
+    // case 3: It lacks digits or does not lack letters
+    else {
       msgPass.innerText =
         "Password is invalid. It must contain letters and at least one digit";
       return false;
@@ -66,6 +87,8 @@ function validatePassword(input) {
 // Validate Re-Password
 function validateRePassword(inputPass, inputRePass) {
   const msgrePass = document.getElementById("msg_rePass");
+
+  //Pass and repass must match
   if (pass.value === rePass.value) {
     msgrePass.innerText = "";
     return true;
@@ -85,6 +108,7 @@ const validate = (event) => {
   const validPass = validatePassword(pass);
   const validRePass = validateRePassword(pass, rePass);
 
+  // print data to the screen
   if (
     validEmail === true &&
     validUname === true &&
