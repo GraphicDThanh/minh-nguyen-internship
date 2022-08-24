dabeeb8a8c4bbc9a4811fbbd71134d117db600fd
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-08-24 14:59:10 +0700

    fix: #52 add image before and after

--- javascript/exercise01/the-logo-hijack.js
+++ javascript/exercise01/the-logo-hijack.js
@@ -1,9 +1,12 @@
-const img = document.getElementById('logo');
-const button = document.getElementById('print');
-
 // Find the Google logo and store it in a variable.
-img.src = 'http://www.logostage.com/logos/yahoo.GIF';
-button.style.height = '50px';
+const img = document.getElementById('logo');
+img.width = 400;
+
+// Modify the source of the logo IMG so that it's a Yahoo logo instead.
+img.src = 'https://s.yimg.com/rz/p/yahoo_homepage_en-US_s_f_p_bestfit_homepage.png';
+
+// Find the Google search button and store it in a variable.
+const button = document.getElementById('gbqfba');
 
 // Modify the text of the button so that it says "Yahooo!" instead.
 button.innerHTML = 'Yahoooo!';
