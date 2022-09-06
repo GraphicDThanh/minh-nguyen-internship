652e7c60db90eb480127188cd719d5b7f18dbfda
minhng271 <minhng271@gmail.com>
2022-09-06 11:03:46 +0700

    feat: #57 todo list controller render

---
+++ javascript/big-practice/src/scripts/controllers/todolist.js
@@ -0,0 +1,10 @@
+export default class TodoListController {
+  constructor(model,view) {
+    this.model = model;
+    this.view = view;
+  }
+
+  init () {
+    this.view.renders(this.model.tasks)
+  }
+}
