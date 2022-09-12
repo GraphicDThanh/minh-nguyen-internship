b4d4939277ad31d0772a292db56bd5f8088a183b
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-09-12 16:06:19 +0700

    fix: #60 add task

--- javascript/big-practice/src/scripts/controllers/todolist.js
+++ javascript/big-practice/src/scripts/controllers/todolist.js
@@ -2,11 +2,17 @@ export default class TodoListController {
   constructor(model, view) {
     this.model = model;
     this.view = view;
-
-    // Explicit this binding
-    this.model.bindTodoListChanged(this.onTodoListChanged);
-    this.view.bindAddTodo(this.handleAddTodo);
   }
-  
-  
+
+  init() {
+    // Explicit this binding
+    // this.view.bindAddTodo(this.onAddTask);
+    this.view.displayTaskList();
+  }
+
+  // Handle add task
+  onAddTask(todoText) {
+    this.model.addTodo(todoText);
+    this.view.displayTaskList(this.model.todos);
+  }
 }
