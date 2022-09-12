ac370f72cd0579f60dfda6779ffa5ceaf4ca1afa
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-09-12 16:04:13 +0700

    fix: #60 add document

--- javascript/big-practice/src/scripts/views/todo.js
+++ javascript/big-practice/src/scripts/views/todo.js
@@ -1,25 +1,25 @@
 export default class TodoItemView {
   constructor() {
-    this.todoList = getElementsByClassName("todo-list");
-    this.addTaskForm = getElementsByClassName("add-task");
-    this.inputTaskName = getElementsByClassName("new-todo");
+    this.todoList = document.getElementsByClassName('todo-list');
+    this.addTaskForm = document.getElementsByClassName('add-task');
+    this.inputTaskName = document.getElementsByClassName('new-todo');
   }
 
   /**
    * Render a task view
    * @param {object} task
-   * @param {number} selectedTaskId
    * @returns task <li> element
    */
 
-  renderTask(task, selectedTaskId) {
-    const taskElement = createElement("li");
+  renderTask(task) {
+    const taskElement = document.createElement('li');
     taskElement.id = `${task.id}`;
-    taskElement.className = `task-item ${task.id == selectedTaskId ? ' selected' : ''}`;
 
     taskElement.innerHTML = `
       <div class = 'view'>
-        <input class='check-box' id='${task.id}-check-box' type='checkbox' ${task.taskDone ? 'checked' : ''}/>
+        <input class='check-box' id='${task.id}-check-box' type='checkbox' ${
+      task.taskDone ? 'checked' : ''
+    }/>
         <label>${task.taskName}</label>
         <button class="destroy"></button>
       </div>
