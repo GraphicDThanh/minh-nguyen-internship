a39e2001e17d185ad4fa9b9a69ee5bb3a1c9861c
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-09-14 19:30:25 +0700

    feat: #62 create feature delete task

--- javascript/big-practice/src/scripts/models/toDoListModel.js
+++ javascript/big-practice/src/scripts/models/toDoListModel.js
@@ -21,7 +21,7 @@ export default class TodoListModel {
     ];
   }
 
-  // add todo
+  // add task todo
   addTodo(todoText) {
     const todoAdded = {
       id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
@@ -30,5 +30,16 @@ export default class TodoListModel {
     };
     const task = new TodoItemModel(todoAdded);
     this.todos.push(task);
+  }
+
+  // delete task
+  deleteTodo(id) {
+    this.taskListModel.forEach((todos) => {
+      if (todos.id === id) {
+        if (this.selectedTask === id) {
+          this.selectedTask = null;
+        }
+      }
+    });
   }
 }
