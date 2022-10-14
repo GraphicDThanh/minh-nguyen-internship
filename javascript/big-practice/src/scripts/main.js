0243c7e1d8539e49e45c29b016cde4e029fb2b13
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-10-14 14:30:49 +0700

    chore: #79 rename file

--- javascript/big-practice/src/scripts/main.js
+++ javascript/big-practice/src/scripts/main.js
@@ -1,8 +1,8 @@
 // import todo list file
 import TodoItemView from './views/todoItemView';
 import TodoListView from './views/todoListView';
-import TodoListModel from './models/toDoListModel';
-import TodoListController from './controllers/toDoListController';
+import TodoListModel from './models/todoListModel';
+import TodoListController from './controllers/todoListController';
 
 const todoListModel = new TodoListModel();
 
