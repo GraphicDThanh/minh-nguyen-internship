2bf36d0bbea8295605549f17eecd73a04146f433
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-10-31 00:17:42 +0700

    style: #97 create space after create variable

--- javascript/big-practice/src/scripts/models/toDoListModel.js
+++ javascript/big-practice/src/scripts/models/toDoListModel.js
@@ -13,15 +13,19 @@ export default class TodoListModel {
   // Save data from localStorage into array
   // getTaskListModel(tasks) {
   //   const todos = [];
+
   //   if (tasks) {
   //     tasks.forEach((task) => {
   //       const taskInstance = new TodoItemModel(task);
+
   //       todos.push(taskInstance);
   //     });
   //   } else if (this.taskListData.getItemLocalStorage()) {
   //     const tasks = this.taskListData.getItemLocalStorage();
+
   //     tasks.forEach((task) => {
   //       const todo = new TodoItemModel(task);
+
   //       todos.push(todo);
   //     });
   //   }
@@ -31,12 +35,14 @@ export default class TodoListModel {
   // Count task active
   async countTaskActive() {
     const todos = await this.getTodo();
+
     return todos.filter((task) => !task.isCompleted).length;
   }
 
   // Count task active
   async countTaskCompleted() {
     const todos = await this.getTodo();
+
     return todos.filter((task) => task.isCompleted).length;
   }
 
@@ -48,6 +54,7 @@ export default class TodoListModel {
       isCompleted: false,
     };
     const task = new TodoItemModel(todoAdded);
+
     await create(task);
   }
 
@@ -55,8 +62,8 @@ export default class TodoListModel {
   async deleteTodo(id) {
     const idNumber = id;
     const index = this.todos.findIndex((task) => task.id === idNumber);
-    this.todos.splice(index, 1);
 
+    this.todos.splice(index, 1);
     await remove(id);
   }
 
@@ -98,11 +105,13 @@ export default class TodoListModel {
     if (filter === 'completed') {
       this.filterType = filter;
       const completedTask = listTodos.filter((task) => task.isCompleted);
+
       return completedTask;
     }
     if (filter === 'active') {
       this.filterType = filter;
       const activeTask = listTodos.filter((task) => !task.isCompleted);
+
       return activeTask;
     }
     return listTodos;
@@ -110,9 +119,9 @@ export default class TodoListModel {
 
   // Delete completed task
   deleteCompletedTodos() {
-    const newTodos = this.todos.filter((task) => task.isCompleted);
+    const listTodosCompleted = this.todos.filter((task) => task.isCompleted);
 
-    newTodos.forEach(async (todo) => {
+    listTodosCompleted.forEach(async (todo) => {
       await remove(todo.id);
     });
   }
@@ -123,6 +132,7 @@ export default class TodoListModel {
    */
   async getTodo() {
     const listTodos = await get();
+
     this.todos = listTodos;
     return listTodos;
   }
