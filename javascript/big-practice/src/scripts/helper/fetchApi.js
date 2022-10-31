d47732e136567ec6e8d604214819f8c0b267248c
minhng271 <minhng271@gmail.com>
2022-10-31 17:41:45 +0700

    fix: #81 update url

--- javascript/big-practice/src/scripts/helper/fetchApi.js
+++ javascript/big-practice/src/scripts/helper/fetchApi.js
@@ -1,12 +1,14 @@
 /**
  * FETCH API
  */
-const URL = 'http://localhost:3000/tasks';
+const URL = 'http://localhost:3000/users';
 
 /**
  * Get all data from JSON server
- * @returns array which is parsed from json
- */
+ * @param {string} url
+ *
+ * @returns {{object} || raise {error}}
+ * */
 export const get = async () => {
   try {
     const response = await fetch(URL);
@@ -23,26 +25,48 @@ export const get = async () => {
 };
 
 /**
- * Add new task to JSON server
- * @param {object} data
+ * Get data user with specific id in database
+ * @param {string} url
+ * @param {number} id
+ *
+ * @returns {{object} || raise {error}}
  */
-export const create = async (data) => {
+
+export const getUserById = async (id) => {
   try {
-    const response = await fetch(URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-    if (!response.ok) {
-      throw response.statusText;
+    const response = await fetch(`${URL}/${id}`);
+    if (response.ok) {
+      const user = await response.json();
+      return user;
     }
+    throw response.statusText;
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
+
+/**
+ * Add new task to JSON server
+ * @param {object} data
+ */
+// export const create = async (data) => {
+//   try {
+//     const response = await fetch(URL, {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(data),
+//     });
+//     if (!response.ok) {
+//       throw response.statusText;
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
 
 /**
  * Update data tasks with specific id in database
@@ -72,20 +96,20 @@ export const update = async (id, data) => {
  * @param {object} data
  * @param {number} id
  */
-export const remove = async (id, data) => {
-  try {
-    const response = await fetch(`${URL}/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-    if (!response.ok) {
-      throw response.statusText;
-    }
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
+// export const remove = async (id, data) => {
+//   try {
+//     const response = await fetch(`${URL}/${id}`, {
+//       method: 'DELETE',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(data),
+//     });
+//     if (!response.ok) {
+//       throw response.statusText;
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
