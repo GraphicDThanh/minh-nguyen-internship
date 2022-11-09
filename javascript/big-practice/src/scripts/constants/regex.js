b7ec3a22a183260dbdce2c4d2835e114bbbea931
minhng271 <49194444+minhng271@users.noreply.github.com>
2022-11-09 22:34:09 +0700

    fix: #107 update redux

--- javascript/big-practice/src/scripts/constants/regex.js
+++ javascript/big-practice/src/scripts/constants/regex.js
@@ -1,6 +1,6 @@
 const REDUX = {
-  REDUX_MAIL: '/^[w-\\.]+@([w-]+.)+[w-]{2,4}$/g',
-  REDUX_PASSWORD: '/^(?=.*[A-Za-z])(?=.*d)[A-Za-zd]{1,}$/g',
+  REDUX_MAIL: /^[\w-\\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+  REDUX_PASSWORD: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{1,}$/g,
 };
 
 export default REDUX;
